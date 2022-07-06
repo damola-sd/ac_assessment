ac456c3d0ef9b70d787032b7aaa278eab1b44fe8
Damola Adewunmi <damolasd@gmail.com>
2022-07-06 23:38:16 +0100

    added next image domains to config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["media.autochek.africa", "storage.googleapis.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
