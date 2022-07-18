8e407a3ee20b305c4a48a71c4bee71292a458565
Damola Adewunmi <damolasd@gmail.com>
2022-07-18 07:19:35 +0100

    added webpack debug

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  webpack(config) {
+    config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
+  },
   reactStrictMode: true,
   images: {
     domains: ["media.autochek.africa", "storage.googleapis.com"],
